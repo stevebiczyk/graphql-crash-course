88b3accfe061641508d25e115253f06117b46527
stevebiczyk <stevebiczyk@gmail.com>
2023-11-23 21:56:59 +0000

    lesson-6

--- index.js
+++ index.js
@@ -12,11 +12,22 @@ const resolvers = {
     games() {
       return db.games;
     },
+
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
     reviews() {
       return db.reviews;
     },
-    authors() {
-      return db.authors;
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
     },
   },
 };
